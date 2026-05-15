9937cfab35c96616c02646afb31f5c8805b3df4b
Nishanth <nishanthcs06@gmail.com>
2026-05-15 12:55:37 +0530

    Fix MongoDB connection

--- server/server.js
+++ server/server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
 const app = express();
 const modelRoutes = require("./routes/modelRoutes");
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -16,10 +14,12 @@ app.get("/", (req, res) => {
 
 app.use("/api", modelRoutes);
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
